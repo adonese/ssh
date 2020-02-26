e0faf3953254c37d337f03c64dbd212f5bbfef2c
Mohamed Yousif <mmbusif@gmail.com>
2020-02-26 22:49:24 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"bytes"
+	"flag"
+)
+
+func main(){
+	//var hostKey ssh.PublicKey
+	// An SSH client is represented with a ClientConn.
+	//
+	// To authenticate with the remote server you must pass at least one
+	// implementation of AuthMethod via the Auth field in ClientConfig,
+	// and provide a HostKeyCallback.
+	
+	hostname := flag.String("host", "", "SSH remote host")
+	port := flag.String("port", "20", "SSH remote port")
+
+	username := flag.String("user", "ubuntu", "Remote hostname")
+	sshPassword := flag.String("ssh-password", "", "Remote SSH password")
+
+	//password := flag.String("user-password", "", "Remote user password")
+	cmd := flag.String("cmd", "ls", "Commands to run during ssh session")
+
+	flag.Parse()
+	
+	config := &ssh.ClientConfig{
+		User: *username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(*sshPassword),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	client, err := ssh.Dial("tcp", *hostname + ":" + *port, config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+
+	// Each ClientConn can support multiple interactive sessions,
+	// represented by a Session.
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer session.Close()
+
+	// Once a Session is created, you can execute a single command on
+	// the remote side using the Run method.
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run(*cmd); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+}
